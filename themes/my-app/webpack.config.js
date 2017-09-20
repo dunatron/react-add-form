3b3a18e8d2a2055f78449bbf643296f36319a058
Heath Dunlop <Tron@Heaths-iMac.lan>
2017-09-20 21:47:14 +1200

    added basic webpack config and dependencies to compile react-create-app out of the box

---
+++ themes/my-app/webpack.config.js
@@ -0,0 +1,77 @@
+const webpack = require('webpack');
+const path = require('path');
+
+var srcPath  = path.join(__dirname, '/src/'),
+    distPath = path.join(__dirname, '/dist/');
+
+module.exports = {
+    watch: true,
+    cache: true,
+    context: srcPath,
+    entry: {
+        app: './index.js',
+    },
+    output: {
+        path: distPath,
+        filename: '[name].bundle.js',
+    },
+    resolve: {
+        modules: ["node_modules"],
+    },
+    plugins: [
+
+    ],
+
+
+    module: {
+
+
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015', 'stage-2', 'react'] },
+                }],
+            },
+            {
+                test: /\.jsx$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015', 'stage-2', 'react'] },
+                }],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], //Loaders are
+                // processed in reverse array order. That means css-loader will run before style-loader.
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            // Loaders for other file types go here
+            { test: /\.(woff|woff2|ttf|eot|svg|gif|png)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+                loader: 'url-loader?limit=100000&name=fonts/[name].[ext]',
+            },
+        ],
+
+
+        loaders: [
+
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            }
+
+        ]
+
+
+    }
+};
